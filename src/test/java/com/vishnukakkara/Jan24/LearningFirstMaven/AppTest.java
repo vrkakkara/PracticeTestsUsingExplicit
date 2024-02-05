1b106fa5a4614ecbd0e8ca3b04b9910b0ea38701
vrkakkara <vishnurajkakkara@gmail.com>
2024-02-05 00:47:04 -0500

    Assignment Feb01

---
+++ src/test/java/com/vishnukakkara/Jan24/LearningFirstMaven/AppTest.java
@@ -0,0 +1,20 @@
+package com.vishnukakkara.Jan24.LearningFirstMavenProject;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
